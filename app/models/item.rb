ca3a08e24dc494c31c204b285170c78f56ef4dd5
miotaro <miomio.pooh.1224@gmail.com>
2024-01-16 16:54:56 +0900

    バリデーションの設定・商品出品ページの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
+  validates :product_name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_time_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :price, presence: true
+
+  belongs_to :user
+
+
   belongs_to :user
   has_one_attached :image
 
