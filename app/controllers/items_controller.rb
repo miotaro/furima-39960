9ae42b21e66b9f6e0e46e2576ada61a024378d8a
miotaro <miomio.pooh.1224@gmail.com>
2024-01-31 18:22:52 +0900

    指摘箇所の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_signin, only: [:edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_signin, only: [:edit, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
