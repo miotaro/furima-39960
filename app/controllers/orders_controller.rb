2ea28264dd1545292d059c86ad2e10c7c05faa5f
miotaro <miomio.pooh.1224@gmail.com>
2024-02-15 15:51:15 +0900

    tokenの実装〜環境変数設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,15 +5,18 @@ class OrdersController < ApplicationController
   before_action :move_to_page
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_address = OrderAddress.new
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -25,7 +28,8 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order_address).permit(:post_code, :prefecture_id, :municipalities, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])  end
+    params.require(:order_address).permit(:post_code, :prefecture_id, :municipalities, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])  
+  end
 
   def move_to_signin
     unless user_signed_in?
@@ -37,9 +41,17 @@ class OrdersController < ApplicationController
     if current_user.id == @item.user_id
       redirect_to root_path
     end
-    if @item.present?
+    if @order_address.present?
       redirect_to root_path
     end
   end
   
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'                 # メモ：通貨の種類（日本円）
+    )
+  end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :municipalities, :street_address, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :municipalities, :street_address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
@@ -8,6 +8,7 @@ class OrderAddress
     validates :street_address
     validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input only number'}, length: { minimum: 10, message: 'is too short'}
     validates :user_id
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
